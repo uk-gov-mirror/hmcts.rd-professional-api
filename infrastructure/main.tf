114f3c734666a57918266d9f6bbb1bf9f8cfbeca
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-09-02 12:47:26 +0100

    Removed unnecessary TF config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,20 +4,20 @@ provider "azurerm" {
 }
 
 locals {
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  preview_app_service_plan = "${var.product}-${var.component}-${var.env}"
-  non_preview_app_service_plan = "${var.product}-${var.env}"
-  app_service_plan = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
+  # local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  # preview_app_service_plan = "${var.product}-${var.component}-${var.env}"
+  # non_preview_app_service_plan = "${var.product}-${var.env}"
+  # app_service_plan = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
 
   preview_vault_name = "${var.raw_product}-aat"
   non_preview_vault_name = "${var.raw_product}-${var.env}"
   key_vault_name = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
 
-  s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  s2s_vault_name = "s2s-${local.local_env}"
-  s2s_vault_uri = "https://s2s-${local.local_env}.vault.azure.net/"
-  idam_url = "https://idam-api.${local.local_env}.platform.hmcts.net"
-  USER_PROFILE_URL = "http://rd-user-profile-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  # s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  # s2s_vault_name = "s2s-${local.local_env}"
+  # s2s_vault_uri = "https://s2s-${local.local_env}.vault.azure.net/"
+  # idam_url = "https://idam-api.${local.local_env}.platform.hmcts.net"
+  # USER_PROFILE_URL = "http://rd-user-profile-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
 }
 
 data "azurerm_key_vault" "rd_key_vault" {
@@ -25,72 +25,72 @@ data "azurerm_key_vault" "rd_key_vault" {
   resource_group_name = "${local.key_vault_name}"
 }
 
-data "azurerm_key_vault" "s2s_key_vault" {
-  name = "s2s-${local.local_env}"
-  resource_group_name = "rpe-service-auth-provider-${local.local_env}"
-}
+# data "azurerm_key_vault" "s2s_key_vault" {
+#   name = "s2s-${local.local_env}"
+#   resource_group_name = "rpe-service-auth-provider-${local.local_env}"
+# }
 
-data "azurerm_key_vault_secret" "s2s_microservice" {
-  name = "s2s-microservice"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "s2s_microservice" {
+#   name = "s2s-microservice"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "s2s_url" {
-  name = "s2s-url"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "s2s_url" {
+#   name = "s2s-url"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "idam_url" {
-  name = "idam-url"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "idam_url" {
+#   name = "idam-url"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "OIDC_ISSUER_URL" {
-  name = "OIDC-ISSUER-URL"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "OIDC_ISSUER_URL" {
+#   name = "OIDC-ISSUER-URL"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "OPEN_ID_API_BASE_URI" {
-  name = "OPEN-ID-API-BASE-URI"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "OPEN_ID_API_BASE_URI" {
+#   name = "OPEN-ID-API-BASE-URI"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
 
-data "azurerm_key_vault_secret" "USER_PROFILE_URL" {
- name = "USER-PROFILE-URL"
- key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "USER_PROFILE_URL" {
+#  name = "USER-PROFILE-URL"
+#  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "CCD_URL" {
- name = "CCD-URL"
- key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "CCD_URL" {
+#  name = "CCD-URL"
+#  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "s2s_secret" {
-  name = "microservicekey-rd-professional-api"
-  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "s2s_secret" {
+#   name = "microservicekey-rd-professional-api"
+#   key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "oauth2_redirect_uri" {
-  name = "OAUTH2-REDIRECT-URI"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "oauth2_redirect_uri" {
+#   name = "OAUTH2-REDIRECT-URI"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "oauth2_client_id" {
-  name = "OAUTH2-CLIENT-ID"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "oauth2_client_id" {
+#   name = "OAUTH2-CLIENT-ID"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-data "azurerm_key_vault_secret" "oauth2_client_secret" {
-  name = "OAUTH2-CLIENT-SECRET"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# data "azurerm_key_vault_secret" "oauth2_client_secret" {
+#   name = "OAUTH2-CLIENT-SECRET"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name      = "${var.component}-POSTGRES-USER"
-  value     = "${module.db-professional-ref-data.user_name}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+#   name      = "${var.component}-POSTGRES-USER"
+#   value     = "${module.db-professional-ref-data.user_name}"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name      = "${var.component}-POSTGRES-PASS"
@@ -98,33 +98,33 @@ resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name      = "${var.component}-POSTGRES-HOST"
-  value     = "${module.db-professional-ref-data.host_name}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+#   name      = "${var.component}-POSTGRES-HOST"
+#   value     = "${module.db-professional-ref-data.host_name}"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name      = "${var.component}-POSTGRES-PORT"
-  value     = "5432"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+#   name      = "${var.component}-POSTGRES-PORT"
+#   value     = "5432"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name      = "${var.component}-POSTGRES-DATABASE"
-  value     = "${module.db-professional-ref-data.postgresql_database}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+#   name      = "${var.component}-POSTGRES-DATABASE"
+#   value     = "${module.db-professional-ref-data.postgresql_database}"
+#   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+# }
 
-resource "azurerm_resource_group" "rg" {
-  name = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
-  tags {
-    "Deployment Environment" = "${var.env}"
-    "Team Name" = "${var.team_name}"
-    "lastUpdated" = "${timestamp()}"
-  }
-}
+# resource "azurerm_resource_group" "rg" {
+#   name = "${var.product}-${var.component}-${var.env}"
+#   location = "${var.location}"
+#   tags {
+#     "Deployment Environment" = "${var.env}"
+#     "Team Name" = "${var.team_name}"
+#     "lastUpdated" = "${timestamp()}"
+#   }
+# }
 
 module "db-professional-ref-data" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,16 +1,16 @@
 
-output "resourceGroup" {
-  value = "${azurerm_resource_group.rg.name}"
-}
+# output "resourceGroup" {
+#   value = "${azurerm_resource_group.rg.name}"
+# }
 
-output "appServicePlan" {
-  value = "${local.app_service_plan}"
-}
+# output "appServicePlan" {
+#   value = "${local.app_service_plan}"
+# }
 
-output "vaultName" {
-  value = "${local.key_vault_name}"
-}
+# output "vaultName" {
+#   value = "${local.key_vault_name}"
+# }
 
-output "vaultUri" {
-  value = "${local.s2s_vault_uri}"
-}
+# output "vaultUri" {
+#   value = "${local.s2s_vault_uri}"
+# }
